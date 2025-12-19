9289cc526cfd50d683056fd7aca96778078ac076
Kuldeepak Gupta <gupta.mag@gmail.com>
2025-12-20 00:30:29 +0530

    Recursion Pt 2

---
+++ src/main/scala/com/dsa/recursion/TowerOfHanoi.scala
@@ -0,0 +1,16 @@
+package com.dsa.recursion
+
+object TowerOfHanoi extends App {
+
+  def getTOH(totalDisk: Int, source: String, medium: String, destination: String): Unit = {
+    if(totalDisk == 1) println(s"Move Disk 1 from ${source} to ${destination}")
+    else {
+      getTOH(totalDisk - 1, source, destination, medium)
+      println(s"Move Disk ${totalDisk} from ${source} to ${destination}")
+      getTOH(totalDisk - 1, medium, source, destination)
+    }
+  }
+
+  getTOH(3, "Source", "Auxiliary", "Destination")
+
+}
